5173bd73e82ba7c7db6faf760415820d6778be26
Mark Goertz <58692211+markgoertz@users.noreply.github.com>
2022-11-03 10:31:43 +0100

    Made Routes within front-end app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import './App.scss'
+import 'swiper/swiper.min.css';
+import './App.scss';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Login from './components/login/Login';
+import Footer from './components/footer/Footer';
+
+import NavRoutes from './config/NavRoutes';
 
 function App() {
     return (
-        <div>
-            App
-        </div>
+        <BrowserRouter>
+            <Route render={props => (
+                <>
+                    <Header {...props}/>
+                    <Login/>
+                    <NavRoutes/>
+                    <Footer/>
+                </>
+            )}/>
+        </BrowserRouter>
     );
 }
-  
+
 export default App;
