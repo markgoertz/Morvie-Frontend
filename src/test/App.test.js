39a7b0281a89e5b605d181f6f8a6c188d24f7d87
Mark Goertz <434548@student.fontys.nl>
2022-12-16 14:19:07 +0100

    Edited test file and CI-pipeline

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -5,21 +5,3 @@ test("Initialize testing with Jest and React!", () => {
   expect("Hello world").toBe("Hello world");
 });
 
-describe("App.js", () => {
-  let browser;
-  let page;
-
-  beforeAll(async () => {
-    browser = await puppeteer.launch();
-    page = await browser.newPage();
-  });
-
-  it("contains the welcome text", async () => {
-    await page.goto("http://localhost:3000");
-    await page.waitForSelector("MORVIE");
-    const text = await page.$eval("MORVIE", (e) => e.textContent);
-    expect(text).toContain("MORVIE");
-  });
-
-  afterAll(() => browser.close());
-});
