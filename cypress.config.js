a5756965fb203ed48a1151250d5761cb490ff206
Mark Goertz <434548@student.fontys.nl>
2022-12-18 12:37:01 +0100

    Reinstalled Cloud option of Cypress.io

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'k4eprr',
+  projectId: "bwgyo4",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
