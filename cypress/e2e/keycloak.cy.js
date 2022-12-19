99363cd1b61f6f17b62e37a0de204fadd738c244
Mark Goertz <58692211+markgoertz@users.noreply.github.com>
2022-12-19 10:46:20 +0100

    Update keycloak test to Localhost URL📃

--- cypress/e2e/keycloak.cy.js
+++ cypress/e2e/keycloak.cy.js
@@ -14,7 +14,7 @@ describe("Visit docker app", () => {
 
 describe("Keycloak-pages", () => {
   it("Access Keycloak registry page", () => {
-    cy.visit("http://host.docker.internal:2222/realms/Morvie/protocol/openid-connect/auth?client_id=MyApp&redirect_uri=https%3A%2F%2Fmorvie-frontend-markgoertz.vercel.app%2F", () => {
+    cy.visit("http://localhost:2222/realms/Morvie/protocol/openid-connect/auth?client_id=MyApp&redirect_uri=https%3A%2F%2Fmorvie-frontend-markgoertz.vercel.app%2F", () => {
       cy.get("a").contains("Register").click();
     });
   });
